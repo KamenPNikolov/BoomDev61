9a758c51f3e3883d8bb858db30fc04a3f7909af2
knikolov <kami_985@abv.bg>
2021-12-22 17:56:27 +0200

    Second

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,13 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  
+    let priceElements = document.querySelectorAll(".hot");
+    for(let element of priceElements){
+     // let newElement = document.createTextNode("🔥");
+      let price = element.innerHTml;
+      element.innerHTML = price + "🔥";
+      //element.parentNode.insertBefore(newElement, element.nextSibling)
+
+    }
 });
